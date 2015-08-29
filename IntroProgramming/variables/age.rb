fe598407a2cc94aa7b55af4b80dfdd3cd64a2b4e
Ryan Reyes <reyesr3@gmail.com>
2015-08-29 04:48:56 +0000

    Add Variables Chapter exercises

---
+++ IntroProgramming/variables/age.rb
@@ -0,0 +1,12 @@
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "In ten years you will be:" 
+puts age + 10 
+
+puts "In twenty years you wil be:"
+puts age + 20 
+
+puts "In thirty years you wil be"
+puts age + 30 
+
---
+++ IntroProgramming/variables/name.rb
@@ -0,0 +1,11 @@
+puts "What's your first name?"
+first_name = gets.chomp
+
+puts "What's your last name?"
+last_name = gets.chomp
+
+puts first_name + ' ' + last_name
+
+10.times do
+  puts first_name + ' ' + last_name
+end
