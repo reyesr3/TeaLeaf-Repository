42b2dd5741fee3f18df128a27477610be8dfccc9
Ryan Reyes <reyesr3@gmail.com>
2015-08-29 04:34:20 +0000

    Add Basics Chapter Exercises

---
+++ IntroProgramming/basics/basics_ex3.rb
@@ -0,0 +1,6 @@
+movies = { Star_Wars: 1970, 
+          Indiana_Jones: 1985, 
+          Up: 2012 }
+puts movies[:Star_Wars]
+puts movies[:Indiana_Jones]
+puts movies[:Up]
---
+++ IntroProgramming/basics/basics_ex4.rb
@@ -0,0 +1,5 @@
+dates = [1970, 1985, 2012]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
