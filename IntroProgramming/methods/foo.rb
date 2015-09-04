3ea293d124017240b12dcff5676bf944da742004
Ryan Reyes <reyesr3@gmail.com>
2015-09-04 04:35:13 +0000

    Add Methods Chapter Exercises

---
+++ IntroProgramming/methods/foo.rb
@@ -0,0 +1,7 @@
+def just_assignment(number)
+  foo = number + 3
+end
+
+puts just_assignment(2)
+
+
---
+++ IntroProgramming/methods/methods_ex3.rb
@@ -0,0 +1,13 @@
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def multiply(a, b)
+  a * b
+end
+
+puts multiply(add(1, 5), subtract(5, 1))
